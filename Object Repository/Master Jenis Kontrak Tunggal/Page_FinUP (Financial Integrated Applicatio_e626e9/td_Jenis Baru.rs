ce03a280c37cb92c24800fb08e8cf040f32777dc
dixxi <dixxi@AbilSayuri_17>
2024-08-04 14:48:34 +0700

    new

---
+++ Object Repository/Master Jenis Kontrak Tunggal/Page_FinUP (Financial Integrated Applicatio_e626e9/td_Jenis Baru.rs
@@ -0,0 +1,119 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>td_Jenis Baru</name>
+   <tag></tag>
+   <elementGuidId>cda71f31-80ad-47c9-bfc0-3d1f7d66f946</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>CSS</key>
+         <value></value>
+      </entry>
+      <entry>
+         <key>XPATH</key>
+         <value>//table[@id='pr_id_15-table']/tbody/tr/td[2]</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>true</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>td</value>
+      <webElementGuid>48a0f62e-e50d-4688-9ec2-602d80cf531c</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>class</name>
+      <type>Main</type>
+      <value>py-0</value>
+      <webElementGuid>b8e0f6d9-337a-4a82-b3ac-e3f86f51592e</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>text</name>
+      <type>Main</type>
+      <value>
+              
+                  Jenis Baru
+                
+            </value>
+      <webElementGuid>4cc88bf3-9b4a-4ffa-a986-3bee5fb6beb8</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>id(&quot;pr_id_15-table&quot;)/tbody[@class=&quot;p-element p-datatable-tbody&quot;]/tr[@class=&quot;p-element p-selectable-row ng-star-inserted&quot;]/td[@class=&quot;py-0&quot;]</value>
+      <webElementGuid>dd26f346-37aa-438b-b086-20c7ed3925d8</webElementGuid>
+   </webElementProperties>
+   <webElementXpaths>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:idRelative</name>
+      <type>Main</type>
+      <value>//table[@id='pr_id_15-table']/tbody/tr/td[2]</value>
+      <webElementGuid>91939c52-f661-42bc-adc2-f86630a02aca</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Jenis Kontrak Tunggal'])[3]/following::td[2]</value>
+      <webElementGuid>3f48ca82-525c-4620-91c8-b8c821eed936</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='ID'])[1]/following::td[2]</value>
+      <webElementGuid>e3bfa645-647d-4f7a-8f0f-7f9b24c8d2d0</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Jenis Baru'])[2]/preceding::td[3]</value>
+      <webElementGuid>1f6a1984-2e13-4166-ae85-3b62a876d099</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:position</name>
+      <type>Main</type>
+      <value>//td[2]</value>
+      <webElementGuid>811d3e6b-48e2-4eb6-9e25-35cdd2bc42dc</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:customAttributes</name>
+      <type>Main</type>
+      <value>//td[(text() = '
+              
+                  Jenis Baru
+                
+            ' or . = '
+              
+                  Jenis Baru
+                
+            ')]</value>
+      <webElementGuid>5343b471-62aa-411f-ac10-f6b356cd7d56</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Test 2'])[1]/preceding::td[3]</value>
+      <webElementGuid>46d6ff87-a979-46bf-8f22-9d35a8c2380e</webElementGuid>
+   </webElementXpaths>
+</WebElementEntity>
